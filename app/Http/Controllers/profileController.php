f9fd03b65c4544963c3f9e5d27a475ec5ada769c
Nafis Iqbal <iqnafis9264@gmail.com>
2023-11-27 17:44:49 +0600

    Module 8 assignment

---
+++ app/Http/Controllers/profileController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class profileController extends Controller
+{
+    //
+
+    public function index(Request $request, $id)
+    { 
+        $name = "Donald Trump";
+        $age = "75";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        return response($data, 200)->cookie(
+            $cookieName,
+            $cookieValue,
+            $minutes,
+            $path,
+            $domain,
+            $secure,
+            $httpOnly
+        );
+    }
+}
